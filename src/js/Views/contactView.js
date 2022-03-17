5645cbaddf1afd59ef4a8448d58f71f3dd3b2c89
Joy Reyes <nethsajo98@gmail.com>
2022-03-17 20:18:55 +0800

    fix layouts

--- src/js/Views/contactView.js
+++ src/js/Views/contactView.js
@@ -7,7 +7,7 @@ class ContactView extends View {
     return `
       <section class="contact" id="contact">
         <div class="wrapper u-t-center">
-          <h2 class="contact__heading heading__secondary u-fw-bold u-mb-sm">
+          <h2 class="contact__heading heading__secondary u-fw-bold u-mb-md">
             ${this._data.heading}
             <span class="u-c-gc">you.</span>
           </h2>
